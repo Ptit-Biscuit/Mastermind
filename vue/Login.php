f0de8f5655b99d076c364d3ff98d88d6f97823cc
Ptit-Biscuit <vincent.brebion@etu.univ-nantes.fr>
2016-12-01 14:34:03 +0100

    ajout d'un peu de css (pour le login)

--- vue/Login.php
+++ vue/Login.php
@@ -9,6 +9,9 @@
 namespace vue;
 
 class Login {
+    /**
+     * Méthode qui affiche la demande du pseudo et du mot de passe
+     */
     public static function displayLogin() {
         ?>
         <!DOCTYPE html>
@@ -16,19 +19,22 @@ class Login {
             <head>
                 <meta charset="UTF-8">
                 <title>Login Mastermind</title>
+                <link rel="stylesheet" type="text/css" href="vue/login.css">
             </head>
 
             <body>
-                <div>
-                    <h1>Authentification</h1>
+                <div class="container">
+                    <div class="login">
+                        <h1>Authentification</h1>
 
-                    <form action="index.php" method="POST">
-                        <input type="text" name="pseudo" placeholder="Identifiant"><br/>
-                        <input type="password" name="password" placeholder="Mot de passe"><br/>
+                        <form action="index.php" method="POST" id="form">
+                            <input type="text" name="pseudo" placeholder="Identifiant"><br/>
+                            <input type="password" name="password" placeholder="Mot de passe"><br/>
 
-                        <input type="submit">
-                        <input type="reset">
-                    </form>
+                            <input type="submit">
+                            <input type="reset">
+                        </form>
+                    </div>
                 </div>
             </body>
         </html>
